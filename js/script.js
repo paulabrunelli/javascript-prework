850dcf7e66304e048291932f0d7c2e9c896992bb
Paula Golczyk <pp.golczyk@gmail.com>
2022-02-23 22:11:31 +0100

    Add script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let computerMove = `kamień`;
 
-printMessage('Zagrałem ' + playerrMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove = `papier`;
 
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
