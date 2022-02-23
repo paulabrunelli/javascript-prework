f7c49378eb8c8614f13ec10d1aec1764e94447fb
Paula Golczyk <pp.golczyk@gmail.com>
2022-02-23 23:22:17 +0100

    Add firstgame

--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
-let computerMove = `kamień`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let computerMove = ``;
+let randomFraction = Math.random();
+let calculation = randomFraction * 3 + 1;
+let roundNumber = Math.floor(calculation);
 
+if (roundNumber==1) {
+    computerMove='kamień';
+}
+else if (roundNumber==2) {
+    computerMove='papier';
+}
+else computerMove='nożyce';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `papier`;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput=='2'){
+    playerMove = 'papier';
+}
+else if (playerInput=='3'){
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+
+else if(computerMove == playerMove){
+    printMessage('Remis!');
+  }
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+  }
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz!');
+  }
+  else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+  }
+  else printMessage('błąd');
